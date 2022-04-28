3889d10ecc172ee16dc8d076a603dae9727b6eab
BuildTools <unconfigured@null.spigotmc.org>
2022-04-29 01:19:23 +0200

    more

--- NcsCommons/src/main/java/com/github/terefang/ncs/common/packet/OpcodeNcsPacketFactory.java
+++ NcsCommons/src/main/java/com/github/terefang/ncs/common/packet/OpcodeNcsPacketFactory.java
@@ -1,5 +1,8 @@
 package com.github.terefang.ncs.common.packet;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+
 public abstract class OpcodeNcsPacketFactory implements NcsPacketFactory
 {
     int opcode;
@@ -11,4 +14,12 @@ public abstract class OpcodeNcsPacketFactory implements NcsPacketFactory
     public void setOpcode(int opcode) {
         this.opcode = opcode;
     }
+
+    // remember there is this opcode (int) in front of the data
+    public abstract NcsPacket unpack(ByteBuf _buf);
+
+    // remember to have the opcode (int) in front of the data
+    public abstract ByteBuf pack(NcsPacket _pkt, ByteBufAllocator alloc);
+
+    public abstract NcsPacket create();
 }
