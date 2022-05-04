b04f7ff60b77a9c711ffccd5423f0acc347c0588
BuildTools <unconfigured@null.spigotmc.org>
2022-05-04 07:03:43 +0200

    correction

--- NcsCommons/src/main/java/com/github/terefang/ncs/common/packet/OpcodeSwitchedNcsPacketFactory.java
+++ NcsCommons/src/main/java/com/github/terefang/ncs/common/packet/OpcodeSwitchedNcsPacketFactory.java
@@ -45,8 +45,8 @@ public class OpcodeSwitchedNcsPacketFactory implements NcsPacketFactory
         // we keep the buffer untouched
         switch (this.opcodeSize)
         {
-            case 1: _opcode = _buf.getByte(0); break;
-            case 2: _opcode = _buf.getShort(0); break;
+            case 1: _opcode = _buf.getByte(0) & 0xff; break;
+            case 2: _opcode = _buf.getShort(0) & 0xffff; break;
             case 4: _opcode = _buf.getInt(0); break;
             default: throw new IllegalArgumentException("illegal opcode size "+this.opcodeSize);
         }
